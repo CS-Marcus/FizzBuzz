7ee3be78d0b267e12c6cfacfd779a59d9d3ae0f1
CS-Marcus <marcusbharrat1@gmail.com>
2025-09-08 21:36:52 -0400

    Final changes

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -4,18 +4,29 @@ public class Multiples{
 
     public static void main(String[] args) {
         System.out.println(multiples(16, 3, 5));
+        System.out.println(multiples());
     }
-
-    static int multiples(int n, int a, int b) {
+    public static int multiples(int n, int a, int b) {
         int counter = 0;
-        for(int i = n - 1; i > 0; i--) {
+        for (int i = n - 1; i > 0; i--) {
             if (i % a == 0 || i % b == 0) {
                 counter++;
 
 
             }
         }
-        return counter ;
+        return counter;
+    }
 
+    public static int multiples() {
+        int counter = 0;
+        for (int i = 999; i > 0; i--) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                counter++;
+
+
+            }
+        }
+        return counter;
     }
 }
